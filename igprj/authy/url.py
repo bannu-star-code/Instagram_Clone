9f07975770e9e44dd7793572f8a2e33bb0a7aca2
Nitin <nitinchiliveri@gmail.com>
2023-01-06 18:41:33 +0530

    Adding the Main Files

---
+++ igprj/authy/url.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from authy.views import UserProfile, EditProfile
+
+
+
+urlpatterns = [
+    # Profile Section
+    path('profile/edit', EditProfile, name="editprofile"),
+]
